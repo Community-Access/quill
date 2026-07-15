234eaf957b9f07b6b0653710d0a299c1a1790c2f
Jeff Bishop <jeff@community-access.org>
2026-07-15 07:58:39 -0700

    release(radio): version 1.0.1 (bug-report identity fix rides this patch)

--- quill/apps/radio.py
+++ quill/apps/radio.py
@@ -20,7 +20,7 @@ from quill.ui.main_frame_radio import RadioMixin
 from quill.ui.main_frame_unlock_codes import UnlockCodesMixin
 
 _TITLE = "Quill Radio"
-_VERSION = "1.0.0"
+_VERSION = "1.0.1"
 _REPO = "Community-Access/quill-radio"
 
 
